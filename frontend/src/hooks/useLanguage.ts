2837d30f19d2f223303e2aad8d4591b1a75c4b98
amanmulani09 <mulaniaman0504@gmail.com>
2026-01-20 22:06:22 +0530

    fix:localization and scope

--- frontend/src/hooks/useLanguage.ts
+++ frontend/src/hooks/useLanguage.ts
@@ -6,17 +6,7 @@ export const languages = [
     { code: 'mr', label: 'मराठी' },
     { code: 'kn', label: 'ಕನ್ನಡ' },
     { code: 'te', label: 'తెలుగు' },
-    { code: 'bn', label: 'বাংলা' },
     { code: 'ta', label: 'தமிழ்' },
-    { code: 'gu', label: 'ગુજરાતી' },
-    { code: 'ml', label: 'മലയാളം' },
-    { code: 'pa', label: 'ਪੰਜਾਬੀ' },
-    { code: 'ur', label: 'اردو' },
-    { code: 'or', label: 'ଓଡ଼ିଆ' },
-    { code: 'as', label: 'অসমীয়া' },
-    { code: 'mni', label: 'মৈতৈলোন্' },
-    { code: 'ne', label: 'नेपाली' },
-    { code: 'gom', label: 'कोंकणी' },
 ];
 
 export const useLanguage = () => {
